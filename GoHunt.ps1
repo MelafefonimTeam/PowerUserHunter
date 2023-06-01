d4fd0616e2555258adf44fe4a868b51d90ca5912
Mr. Cucumber <135000496+MelafefonimTeam@users.noreply.github.com>
2023-06-01 09:54:46 +0300

    Add username parameter, add more filters to the computers variable, fix indentation, remove the ping test connection.

--- GoHunt.ps1
+++ GoHunt.ps1
@@ -1,24 +1,24 @@
 Function Find-UserSession {
-$Username = Read-Host "Enter username you want to search for"
+    [cmdletbinding()]
+    param(
+        [parameter(mandatory)]
+        [string]$Username
+    )
 
-$computers = Get-WmiObject -Namespace root\directory\ldap -Class ds_computer | select ds_cn #add Where-Object {} filter
+    $computers = Get-WmiObject -Namespace root\directory\ldap -Class ds_computer | Where-Object {$_.DS_operatingSystem -eq 'Windows 10 Enterprise' -and $_.DS_lastLogonTimestamp -le ((Get-Date).AddHours(-24)).Ticks} | select ds_cn 
 
-foreach ($comp in $computers)
-	{
-	$Computer = $comp.ds_cn
-	$ping = new-object System.Net.NetworkInformation.Ping
-  	$Reply = $null
-  	$Reply = $ping.send($Computer)
-  	if($Reply.status -like 'Success'){
-		#Get explorer.exe processes
-		$proc = gwmi win32_process -computer $Computer -Filter "Name = 'explorer.exe'"
-		#Search collection of processes for username
-		ForEach ($p in $proc) {
-	    	$temp = ($p.GetOwner()).User
-	  		if ($temp -eq $Username){
-			write-host -ForegroundColor Green "$Username is logged on $Computer"
-		}
-      }
+    foreach ($comp in $computers) {
+        $Computer = $comp.ds_cn
+        Write-Verbose "Try to access $computer"
+        #Get explorer.exe processes
+        $proc = gwmi win32_process -computer $Computer -Filter "Name = 'explorer.exe'" -ErrorAction SilentlyContinue 
+        
+        #Search collection of processes for username
+        ForEach ($p in $proc) {
+            $temp = ($p.GetOwner()).User
+            if ($temp -eq $Username) {
+                write-host -ForegroundColor Green "$Username is logged on $Computer"
+            }
+        }
     }
-  }
 }
